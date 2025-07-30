eb36cb2e9b8900b4b0e4a35e3ad4a16e2ccaa95c
pedro-henrique-silva17 <164204915+pedro-henrique-silva17@users.noreply.github.com>
2025-07-30 11:33:53 -0300

    Update introdução-a-collections.py

--- introdução-a-collections.py
+++ introdução-a-collections.py
@@ -263,3 +263,78 @@ contas[0].deposita(300)
 
 for conta in contas:
   print(conta)
+
+"""# Herança e polimorfismo"""
+
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass=ABCMeta):
+  
+  def __init__(self, codigo):
+    self._codigo = codigo
+    self._saldo = 0
+
+  def deposita(self, valor):
+    self._saldo += valor
+
+  @abstractmethod
+  def passa_o_mes(self):
+    pass
+
+  def __str__(self):
+    return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+print(Conta(88))
+
+class ContaCorrente(Conta):
+  
+  def passa_o_mes(self):
+    self._saldo -= 2
+    
+class ContaPoupanca(Conta):
+  
+  def passa_o_mes(self):
+    self._saldo *= 1.01
+    self._saldo -= 3
+
+class ContaInvestimento(Conta):
+  pass
+
+ContaInvestimento(764)
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta16.passa_o_mes()
+print(conta16)
+
+conta17 = ContaPoupanca(17)
+conta17.deposita(1000)
+conta17.passa_o_mes()
+print(conta17)
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta17 = ContaPoupanca(17)
+conta17.deposita(1000)
+contas = [conta16, conta17]
+
+for conta in contas:
+  conta.passa_o_mes() # duck typing
+  print(conta)
+
+"""# evitaremos usar array puro. se precisamos de trabalho numérico, é costume usar o numpy"""
+
+import array as arr
+
+arr.array('d', [1, 3.5])
+
+arr.array('d', [1, 3.5, 'Guilherme'])
+
+!pip install numpy
+
+import numpy as np
+
+numeros = np.array([1, 3.5])
+numeros
+
+numeros + 3
